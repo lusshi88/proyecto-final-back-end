4df2b98ec6792fe881ccc39c6b42757fb020f07a
Luis <bravoluis439@ymail.com>
2024-02-10 02:01:34 -0300

    se agrega handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const productsRoutes =  require('./routes/products.js');
 const cartRoutes = require('./routes/carts.js');
+const path = require('path');
+const productos = require ("./data/productos.json");
+
+
+const handlebars = require('express-handlebars'); 
+
 
 const PORT = 8080;
 
 const app = express();
+
+// motor de plantillas
+app.engine("handlebars",handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../views"));
+
+// ruta principal
+app.get("/",(req, res) => {
+  res.render ("./layouts/home.handlebars", {productos: productos});
+  });
+  
+
+
 
 app.use(express.json());
 // app.use(express.urlencoded());
